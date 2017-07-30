37e4323ea077be7a620e1a2f5325054fb81ca8c2
Quoc-An Ho <quocanho29@gmail.com>
2017-07-30 23:30:24 +1000

    Add remaining instances to do for PubKey/PrivKey
    Also replaces some extraneous comments, adds a module description.
    TODO: All of these instances, especially for Key still need to be
    reviewed/tested to ensure that they are inline with the go-libp2p-crypto
    implementation

--- src/Crypto/LibP2P/Key.hs
+++ src/Crypto/LibP2P/Key.hs
@@ -1,14 +1,19 @@
-{-# LANGUAGE FunctionalDependencies #-}
+{-|
+Module      : Crypto.LibP2P.Key
+Description : Short description
+Copyright   : (c) Roger Qiu, 2017
+License     : MIT
+Maintainer  : quoc.ho@matrix.ai
+Stability   : experimental
+Portability : POSIX
 
+This module provides the Key typeclass, allowing serialization to and from
+a Protobuf serialized DER format.
+Compare against go-libp2p-crypto/js-libp2p-crypto.
+TODO: Need to review this instance, check if the byte encodings match
+-- with the implementation in go-libp2p-crypto
+-}
 module Crypto.LibP2P.Key where
-
--- this module exposes a typeclass and instances of relevant key algos for libp2p
--- the problem is that the modules exposing the various utilities are not very standardised
--- it's because there are many ways to achieve the same thing
--- like signing for RSA means getting an optional blinder, a maybe hash algorithm, the the private key and finally the bytestring which may return an error
--- however it appears the defaults have been designed into the libp2p library, so for example the sign for rsa private key in go is using a 256 checksum of the bytestring message (should we be using lazy byte strings?) then it uses SignPKCS1v15 with a random readerm a sha256 and finally the hashed array
--- so we are exposing specific ways of making use of these cryptographic algos for libp2p usage
--- also there's the usage of protobuf that we may need
 
 import qualified Crypto.PubKey.Ed25519             as Ed25519
 import qualified Crypto.PubKey.RSA                 as RSA
@@ -17,7 +22,6 @@ import qualified Crypto.Secp256k1                  as Secp256k1
 import qualified Data.ASN1.Encoding                as ASN1Encoding
 import qualified Data.ASN1.Types                   as ASN1Types
 import qualified Data.ByteString                   as BSStrict
---import qualified Data.ByteString.Base64            as Base64
 import qualified Data.ByteString.Lazy              as BSLazy
 import qualified Data.X509                         as X509
 
@@ -26,7 +30,7 @@ import qualified Crypto.LibP2P.Protobuf.PrivateKey as ProtoPrivKey
 import qualified Crypto.LibP2P.Protobuf.PublicKey  as ProtoPubKey
 
 import           Crypto.PubKey.RSA.Types           ()
-import           Data.ASN1.BinaryEncoding          (BER(..), DER (..))
+import           Data.ASN1.BinaryEncoding          (DER (..))
 import           Data.ByteArray                    (convert)
 import           Text.ProtocolBuffers.WireMessage  (messagePut)
 
@@ -43,9 +47,6 @@ instance Key Ed25519.PublicKey where
     $ convert k
   fromBytes = undefined
 
--- not sure if this requires the public key and private key at the same time
--- the go returns always a 96 byte length byte string
--- need an example of a valid bytes to guide this development
 instance Key Ed25519.SecretKey where
   toBytes k =
     BSLazy.toStrict
@@ -54,8 +55,7 @@ instance Key Ed25519.SecretKey where
     $ BSLazy.fromStrict
     $ convert k
   fromBytes = undefined
--- no idea if the go implementation implements DER encoding, which this does
--- the go implementation is also compressed, and we switch on compression here as well
+
 instance Key Secp256k1.PubKey where
   toBytes k =
     BSLazy.toStrict
@@ -65,8 +65,6 @@ instance Key Secp256k1.PubKey where
     $ Secp256k1.exportPubKey True k
   fromBytes = undefined
 
--- this is missing some sort of bitcoin serialisation scheme in the go implementation, I don't understand what that scheme is
--- the byte representation is a strict byte string containing a protobuf encoded type specified by the the proto file
 instance Key Secp256k1.SecKey where
   toBytes k =
     BSLazy.toStrict
@@ -75,54 +73,20 @@ instance Key Secp256k1.SecKey where
     $ BSLazy.fromStrict
     $ Secp256k1.getSecKey k
   fromBytes = undefined
-instance Key X509.PubKey where
-  toBytes k =
-    ASN1Encoding.encodeASN1' DER
-    $ (ASN1Types.toASN1 k) []
-    
-  fromBytes = undefined
 
--- there is no ASN1 object representation of X509 keys at the moment,
--- this should be implemented, see
--- see https://github.com/vincenthz/hs-certificate/issues/55
--- https://github.com/vincenthz/hs-crypto-pubkey-types/pull/12
---
--- instance Key X509.PrivKey where
---   toBytes k =
---     case ASN1Encoding.encodeASN1' DER [toASN1 k] of
---       Right bs -> Base64.encode bs
---       Left e   -> show e
-
+-- We rely on X509 and PKIX infrastructure for the serialization of
+-- RSA public keys
 instance Key RSA.PublicKey where
   toBytes k =
-    toBytes
-    $ X509.PubKeyRSA k
+     ASN1Encoding.encodeASN1' DER
+    $ (ASN1Types.toASN1 $ X509.PubKeyRSA k) []
   fromBytes = undefined
 
+-- However, Private Keys are serialized using the form defined in
+-- PKCS#1 v1.5. Since cryptonite doesn't export the serialization format
+-- at the moment, we add the orphan instance in this library
 instance Key RSA.PrivateKey where
   toBytes k =
     ASN1Encoding.encodeASN1' DER
     $ (ASN1Types.toASN1 k) []
   fromBytes = undefined
-
--- TODO: Instead of using a typeclass, we can use data constructors to wrap
--- the underlying key types as documented below.
--- Not sure which is the better design in this case?
---
--- data Key = PubKey PubKey | PrivKey PrivKey
---   deriving (Eq)
-
--- data PubKey = PubRSA RSA.PublicKey
---            | PubEd25519 Ed25519.PublicKey
---            | PubSecp256k1 Secp256k1.PubKey
---             deriving (Eq)
-
--- data PrivKey = PrivRSA RSA.PrivateKey
---             | PrivEd25519 Ed25519.SecretKey
---             | PrivSecp256k1 Secp256k1.SecKey
---             deriving (Eq)
-
--- toPublic :: PrivKey -> PubKey
--- toPublic (PrivRSA privKey) = PubRSA $ RSA.private_pub privKey
--- toPublic (PrivEd25519 privKey) = PubEd25519 $ Ed25519.toPublic privKey
--- toPublic (PrivSecp256k1 privKey) = PubSecp256k1 $ Secp256k1.derivePubKey privKey
--- src/Crypto/LibP2P/PrivKey.hs
+++ src/Crypto/LibP2P/PrivKey.hs
@@ -5,7 +5,7 @@ Module      : Crypto.LibP2P.PrivKey
 Description : Short description
 Copyright   : (c) Roger Qiu, 2017
 License     : MIT
-Maintainer  : quoc-an.ho@matrix.ai
+Maintainer  : roger.qiu@matrix.ai
 Stability   : experimental
 Portability : POSIX
 
@@ -14,23 +14,42 @@ commentary with @some markup@.
 -}
 module Crypto.LibP2P.PrivKey where
 
-import Crypto.LibP2P.Key
-import Crypto.LibP2P.PubKey
+import           Crypto.LibP2P.Key
+import           Crypto.LibP2P.PubKey
 
-import qualified Crypto.PubKey.Ed25519             as Ed25519
-import qualified Crypto.PubKey.RSA                 as RSA
-import qualified Crypto.Secp256k1                  as Secp256k1
+import qualified Crypto.Hash.Algorithms   as Hash
+import qualified Crypto.PubKey.Ed25519    as Ed25519
+import qualified Crypto.PubKey.RSA        as RSA
+import qualified Crypto.PubKey.RSA.PKCS15 as PKCS15
+import qualified Crypto.Secp256k1         as Secp256k1
 
-import qualified Data.ByteString                   as BSStrict
-import qualified Data.X509                         as X509
+import qualified Data.ByteString          as BS
 
-import           Data.ByteArray                    (convert)
-import           Text.ProtocolBuffers.WireMessage  (messagePut)
+import           Data.ByteArray           (convert)
 
 class (Key a, PubKey b) => PrivKey a b | a -> b where
-  sign :: a -> BSStrict.ByteString -> Either String BSStrict.ByteString
+  sign :: a -> BS.ByteString -> Either String BS.ByteString
   toPublic :: a -> b
 
 instance PrivKey Ed25519.SecretKey Ed25519.PublicKey where
   sign k d = Right $ convert $ Ed25519.sign k (Ed25519.toPublic k) d
   toPublic k = Ed25519.toPublic k
+
+instance PrivKey Secp256k1.SecKey Secp256k1.PubKey where
+  sign k d = case Secp256k1.msg d of
+                  Nothing ->
+                    Left "Failed parsing bytes to Secp256k1.Msg"
+                  Just m ->
+                    Right $ Secp256k1.exportSig $ Secp256k1.signMsg k m
+  toPublic k = Secp256k1.derivePubKey k
+
+-- TODO: Adding a blinder to this computation requires
+-- the introduction of MonadRandom, which means the
+-- type signature of sign will have to be changed.
+-- For now, we omit the blinder, but this should be added
+-- once we figure out the best type signature for this typeclass
+instance PrivKey RSA.PrivateKey RSA.PublicKey where
+  sign k d = case PKCS15.sign Nothing (Just Hash.SHA256) k d of
+                  Left e    -> Left $ show e
+                  Right sig -> Right sig
+  toPublic k = RSA.private_pub k
