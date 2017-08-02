38139090db1a887a08685d92ae1a28b3215d8da7
Quoc-An Ho <quocanho29@gmail.com>
2017-08-02 22:51:17 +1000

    Add deserialize to Key typeclass
    This allows for parsing of a protobuf encoded, serialized version of the
    Key to the ADT of the instance. Also, the functionality to wrap and
    unwrap a protobuf message is abstracted to their own functions

--- src/Crypto/LibP2P/Key.hs
+++ src/Crypto/LibP2P/Key.hs
@@ -8,7 +8,7 @@ Stability   : experimental
 Portability : POSIX
 
 This module provides the Key typeclass, allowing serialization to and from
-a Protobuf serialized DER format.
+a Protobuf serialized format.
 Compare against go-libp2p-crypto/js-libp2p-crypto.
 TODO: Need to review this instance, check if the byte encodings match
 -- with the implementation in go-libp2p-crypto
@@ -21,72 +21,173 @@ import qualified Crypto.Secp256k1                  as Secp256k1
 
 import qualified Data.ASN1.Encoding                as ASN1Encoding
 import qualified Data.ASN1.Types                   as ASN1Types
+import qualified Data.Bifunctor                    as Bifunctor
 import qualified Data.ByteString                   as BSStrict
 import qualified Data.ByteString.Lazy              as BSLazy
 import qualified Data.X509                         as X509
+
 
 import qualified Crypto.LibP2P.Protobuf.KeyType    as ProtoKeyType
 import qualified Crypto.LibP2P.Protobuf.PrivateKey as ProtoPrivKey
 import qualified Crypto.LibP2P.Protobuf.PublicKey  as ProtoPubKey
 
-import           Crypto.PubKey.RSA.Types           ()
+import           Crypto.Error                      (eitherCryptoError)
 import           Data.ASN1.BinaryEncoding          (DER (..))
 import           Data.ByteArray                    (convert)
-import           Text.ProtocolBuffers.WireMessage  (messagePut)
+import           Text.ProtocolBuffers.WireMessage  (messageGet, messagePut)
+
+-- imports ASN1Object instance for RSA.PrivateKey
+import           Crypto.PubKey.RSA.Types           ()
+
 
 class (Eq a) => Key a where
-  toBytes :: a -> BSStrict.ByteString
-  fromBytes :: BSStrict.ByteString -> Either String a
+  serialize :: a -> BSStrict.ByteString
+  deserialize :: BSStrict.ByteString -> Either String a
 
 instance Key Ed25519.PublicKey where
-  toBytes k =
-    BSLazy.toStrict
-    $ messagePut
-    $ ProtoPubKey.PublicKey ProtoKeyType.Ed25519
-    $ BSLazy.fromStrict
+  serialize k =
+    encodeProtoPublic ProtoKeyType.Ed25519
     $ convert k
-  fromBytes = undefined
+
+  deserialize b = decodePublicKey b decodeEd25519Pub
+    where
+      decodeEd25519Pub :: BSStrict.ByteString -> Either String Ed25519.PublicKey
+      decodeEd25519Pub bs =
+        Bifunctor.first show
+        $ eitherCryptoError
+        $ Ed25519.publicKey bs
+
 
 instance Key Ed25519.SecretKey where
-  toBytes k =
-    BSLazy.toStrict
-    $ messagePut
-    $ ProtoPrivKey.PrivateKey ProtoKeyType.Ed25519
-    $ BSLazy.fromStrict
+  serialize k =
+    encodeProtoPublic ProtoKeyType.Ed25519
     $ convert k
-  fromBytes = undefined
+
+  deserialize b = decodePrivateKey b decodeEd25519Sec
+    where
+      decodeEd25519Sec :: BSStrict.ByteString -> Either String Ed25519.SecretKey
+      decodeEd25519Sec bs =
+        Bifunctor.first show
+        $ eitherCryptoError
+        $ Ed25519.secretKey bs
+
 
 instance Key Secp256k1.PubKey where
-  toBytes k =
-    BSLazy.toStrict
-    $ messagePut
-    $ ProtoPubKey.PublicKey ProtoKeyType.Secp256k1
-    $ BSLazy.fromStrict
+  serialize k =
+    encodeProtoPublic ProtoKeyType.Secp256k1
     $ Secp256k1.exportPubKey True k
-  fromBytes = undefined
+
+  deserialize b = decodePublicKey b decodeSecp256k1Pub
+    where
+      decodeSecp256k1Pub :: BSStrict.ByteString -> Either String Secp256k1.PubKey
+      decodeSecp256k1Pub bs =
+        case Secp256k1.importPubKey bs of
+             Nothing -> Left "failed to read Secp256k1.PubKey from bytestring"
+             Just pk -> Right pk
 
 instance Key Secp256k1.SecKey where
-  toBytes k =
-    BSLazy.toStrict
-    $ messagePut
-    $ ProtoPrivKey.PrivateKey ProtoKeyType.Secp256k1
-    $ BSLazy.fromStrict
+  serialize k =
+    encodeProtoPublic ProtoKeyType.Secp256k1
     $ Secp256k1.getSecKey k
-  fromBytes = undefined
+
+  deserialize b = decodePrivateKey b decodeSecp256k1Sec
+    where
+      decodeSecp256k1Sec :: BSStrict.ByteString -> Either String Secp256k1.SecKey
+      decodeSecp256k1Sec bs =
+        case Secp256k1.secKey bs of
+             Nothing -> Left "failed to read Secp256k1.SecKey from bytestring"
+             Just pk -> Right pk
 
 -- We rely on X509 and PKIX infrastructure for the serialization of
 -- RSA public keys
 instance Key RSA.PublicKey where
-  toBytes k =
-     ASN1Encoding.encodeASN1' DER
+  serialize k =
+    encodeProtoPublic ProtoKeyType.RSA
+    $ ASN1Encoding.encodeASN1' DER
     $ (ASN1Types.toASN1 $ X509.PubKeyRSA k) []
-  fromBytes = undefined
 
--- However, Private Keys are serialized using the form defined in
--- PKCS#1 v1.5. Since cryptonite doesn't export the serialization format
--- at the moment, we add the orphan instance in this library
+  deserialize b = decodePublicKey b decodeRSAPub
+    where
+      decodeRSAPub :: BSStrict.ByteString -> Either String RSA.PublicKey
+      decodeRSAPub bs = derToAsn bs >>= asnToX509 >>= x509ToRSA
+
+      asnToX509 :: [ASN1Types.ASN1] -> Either String X509.PubKey
+      asnToX509 asn =
+        Bifunctor.second fst
+        $ ASN1Types.fromASN1 asn
+
+      x509ToRSA :: X509.PubKey -> Either String RSA.PublicKey
+      x509ToRSA (X509.PubKeyRSA k) = Right k
+      x509ToRSA _ = Left "Public key of x509 certificate was not of type RSA"
+
+-- Private Keys are serialized using the form defined in
+-- PKCS#1 v1.5. Since cryptonite doesn't export the serialization format (ASN1)
+-- at the moment, we add the orphan instance in Crypto.PubKey.RSA.Types
+-- of this library
 instance Key RSA.PrivateKey where
-  toBytes k =
-    ASN1Encoding.encodeASN1' DER
+  serialize k =
+    encodeProtoPrivate ProtoKeyType.RSA
+    $ ASN1Encoding.encodeASN1' DER
     $ (ASN1Types.toASN1 k) []
-  fromBytes = undefined
+
+  deserialize b = decodePrivateKey b decodeRSAPriv
+    where
+      decodeRSAPriv :: BSStrict.ByteString -> Either String RSA.PrivateKey
+      decodeRSAPriv bs = derToAsn bs >>= asnToRSA
+
+      asnToRSA :: [ASN1Types.ASN1] -> Either String RSA.PrivateKey
+      asnToRSA asn =
+        Bifunctor.second fst
+        $ ASN1Types.fromASN1 asn
+
+derToAsn :: BSStrict.ByteString -> Either String [ASN1Types.ASN1]
+derToAsn bs =
+  Bifunctor.first show
+  $ ASN1Encoding.decodeASN1' DER bs
+
+encodeProtoPublic :: ProtoKeyType.KeyType ->
+                     BSStrict.ByteString ->
+                     BSStrict.ByteString
+encodeProtoPublic kt bs =
+  BSLazy.toStrict
+  $ messagePut
+  $ ProtoPubKey.PublicKey kt
+  $ BSLazy.fromStrict bs
+
+encodeProtoPrivate ::
+                      ProtoKeyType.KeyType ->
+                      BSStrict.ByteString ->
+                      BSStrict.ByteString
+encodeProtoPrivate kt bs =
+  BSLazy.toStrict
+  $ messagePut
+  $ ProtoPrivKey.PrivateKey kt
+  $ BSLazy.fromStrict bs
+
+-- TODO: decodeProto functions unwrap a protobuf key
+-- and returns a bytestring representing the key as bytes.
+-- There should be a better way to type this function
+-- to include maybe a constructor for a given Key a
+-- but this works for now until we can get a full stack working.
+decodePublicKey :: (Key a) =>
+                    BSStrict.ByteString ->
+                    (BSStrict.ByteString -> Either String a) ->
+                    Either String a
+decodePublicKey bs toKey = decodeProtoPublic bs >>= toKey
+  where
+    decodeProtoPublic bs =
+      Bifunctor.second (BSLazy.toStrict . ProtoPubKey.data' . fst)
+      $ messageGet
+      $ BSLazy.fromStrict bs
+
+decodePrivateKey :: (Key a) =>
+                     BSStrict.ByteString ->
+                     (BSStrict.ByteString -> Either String a) ->
+                     Either String a
+decodePrivateKey bs toKey = decodeProtoPrivate bs >>= toKey
+  where
+    decodeProtoPrivate :: BSStrict.ByteString -> Either String BSStrict.ByteString
+    decodeProtoPrivate bs =
+      Bifunctor.second (BSLazy.toStrict . ProtoPrivKey.data' . fst)
+      $ messageGet
+      $ BSLazy.fromStrict bs
